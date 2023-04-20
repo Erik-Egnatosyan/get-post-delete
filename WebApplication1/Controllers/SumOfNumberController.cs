5ca2b54905751210321633e59e63be8e922eb9e6
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-04-20 16:57:45 +0400

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/SumOfNumberController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumOfNumberController : ControllerBase
+    {
+        private readonly List<int> _numbers = new List<int> { 1, 2, 3 };
+
+        [HttpGet]
+        public IActionResult GetNumbers()
+        {
+            return Ok(_numbers);
+        }
+
+        [HttpPost("numbers")]
+        public IActionResult AddNumber(int number)
+        {
+            _numbers.Add(number);
+            return Ok(_numbers);
+        }
+
+        [HttpDelete]
+        [Route("numbers/{number}")]
+        public IActionResult RemoveNumber(int number)
+        {
+            if (_numbers.Contains(number))
+            {
+                _numbers.Remove(number);
+                return Ok(_numbers);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
